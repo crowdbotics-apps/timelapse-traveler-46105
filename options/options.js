681f8b94cc9c15d0e19fc4670b8e353d4a8214bb
Crowdbotics <team@crowdbotics.com>
2024-02-07 20:12:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "timelapse-traveler-46105",
+  url: "https://timelapse-traveler-46105.botics.co",
+  api: "https://timelapse-traveler-46105.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
